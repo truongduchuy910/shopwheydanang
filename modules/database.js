6827e5b3ec35c9e194efaaa9ad8a38d74018be07
Trần Ngọc Huy <truongduchuy910@gmail.com>
2019-08-25 13:25:14 +0700

    databse

--- modules/database.js
+++ modules/database.js
@@ -3,8 +3,41 @@ module.exports = {
     initialize: function (config) {
         mongoose.connect(config.uri, { useNewUrlParser: true }, (err) => {
             if (!err) {
-                console.log('connected to mongodb');
+                console.log('Kết nối thành công tới mongodb');
             }
         })
-    }
+    },
+    product: {
+        basis: mongoose.model('product.basis', mongoose.Schema(
+            {
+                name: String,
+                price: String,
+                sale: String
+            }
+        )),
+        details: mongoose.model('product.details', mongoose.Schema(
+            {
+                description: Object,
+                guide: Object,
+                detail: Object,
+                taste: Array,
+                brand: Array,
+                function: Array,
+                category: Array
+            }
+        )),
+        attributes: mongoose.model('product.attributes', mongoose.Schema(
+            {
+                taste: Array,
+                brand: Array,
+                function: Array,
+                category: Array
+            }
+        )),
+    },
+    post: mongoose.model('post', mongoose.Schema(
+        {
+            content: Object
+        }
+    ))
 }
